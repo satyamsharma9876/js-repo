3b7217ea52468a1949eef9dddb5fb8abee5777c9
satyamsharma9876 <131748390+satyamsharma9876@users.noreply.github.com>
2025-11-02 05:54:49 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId=144553
+let accountEmail="hitsh@google.com"
+var accountPassword="12345"
+// account id=2 // not allowd
+accountcity="jaipur"
+console.log(accountId);
+accountEmail="hjxdjedkh@google.com"
+accountPassword="17484787395"
+accountcity="bengluru"
+let accountstate
+/*
+prefer not use var b/c of issue in blk scope
+ and functionalscope & const variable cant be changed 
+*/
+console.table([accountId,accountEmail,accountPassword,accountcity, accountstate])
+
