5fce4849d3732bcc56c918d4f33fda72c660028e
satyamsharma9876 <131748390+satyamsharma9876@users.noreply.github.com>
2025-11-02 06:51:41 +0000

    conversions

--- 01_basics/03_conversionsOperations.js
+++ 01_basics/03_conversionsOperations.js
@@ -10,4 +10,17 @@ let isloggedIn=1
 let booleanIsLoggedIn=Boolean(isloggedIn)
 console.log(booleanIsLoggedIn); // if isloggedin is any value o/p is true &if "" o/p is false 
 
+// operations
+let value=3
+let negvalue=-value
+console.log(negvalue);//-3
+console.log(3+3); //6
+console.log(3**3);// and many more operations
 
+let str1="hello"
+let str2="hitesh"
+let str3=str1+str2
+console.log(str3);
+console.log("1"+2); //12
+console.log("1"+2+2);//122
+console.log(1+2+"2"); //32
