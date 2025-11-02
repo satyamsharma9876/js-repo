3629ef6e21e862eecd6f2f0a371f0316aafcabfb
satyamsharma9876 <131748390+satyamsharma9876@users.noreply.github.com>
2025-11-02 07:05:59 +0000

    comparsions

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,19 @@
+console.log(3==1);//f
+
+console.log("2" > 1);// t b/c"2" converted to 2
+console.log("02">1);//t
+console.log(null==0);//t b/c null treated as 0
+console.log(null>=0);//t
+console.log(null==1);//f
+console.log(undefined ==0);//f
+console.log(undefined >0)//f
+
+//=== // it checks datatype also
+console.log("2"===2) //f
+
+
+
+
+
+
+
