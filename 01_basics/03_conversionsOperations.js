cc2d3d5ff245dfe456362da82ac7686a2ad9f5cf
satyamsharma9876 <131748390+satyamsharma9876@users.noreply.github.com>
2025-11-02 06:29:46 +0000

    conversions of datatypes

---
+++ 01_basics/03_conversionsOperations.js
@@ -0,0 +1,13 @@
+let score ="33abc"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber =Number(score); // converting string score into number
+console.log(typeof valueInNumber);
+console.log(valueInNumber); // Nan => not a no. & if score=null o/p=0, if score=undefined o/p=Nan
+
+let isloggedIn=1
+let booleanIsLoggedIn=Boolean(isloggedIn)
+console.log(booleanIsLoggedIn); // if isloggedin is any value o/p is true &if "" o/p is false 
+
+
