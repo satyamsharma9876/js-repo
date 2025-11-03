14ec0c5173f2cc898b53192352f553f7fc33c9d8
satyamsharma9876 <131748390+satyamsharma9876@users.noreply.github.com>
2025-11-03 16:17:13 +0000

    conditionl

---
+++ 04_control_flow/one.js
@@ -0,0 +1,35 @@
+
+//if
+
+//switch
+const month=3// if month is "string" makes cases also string like "jan","feb",etc
+switch(month){
+    case 1: 
+        console.log("j");
+        break;
+    case 2: 
+        console.log("F");
+        break;
+    case 3: 
+        console.log("M");
+        break;
+    default:
+        console.log("nothing matched");
+        break;        
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,8 @@
+
+//falsy values ....means which values r considered to be false
+//false,0,-0,BigInt,0n,"",null,NaN
+
+//truthy values
+//"anything in string is truthy value",eg=>
+// "0",'false'," ",[],function(){}
+
