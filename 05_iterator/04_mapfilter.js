ea7e38b6d590a7e7e2843514236fde66b4f63f38
satyamsharma9876 <131748390+satyamsharma9876@users.noreply.github.com>
2025-11-04 08:37:36 +0000

    filterrr

--- 05_iterator/04_mapfilter.js
+++ 05_iterator/04_mapfilter.js
@@ -23,9 +23,27 @@ myNum.forEach( (num) =>{
 } )
 console.log(newNums);//[ 5, 6, 7, 8, 9, 10 ]
 
+const books=[
+    {
+        title:'Book One', genre: 'Fiction', publish: 1981,edition: 2004
+    },
+    {
+        title:'Book Two', genre: 'Non-Fiction', publish: 1951,edition: 2009
+    },
+    {
+        title:'Book three', genre: 'History', publish: 1961,edition: 2005
+    },
+    {
+        title:'Book four', genre: 'History', publish: 1971,edition: 2003
+    },
+]
+let userbooks=books.filter( (bk) => bk.genre ==='History')
+console.log(userbooks);
 
-
-
+userbooks=books.filter( (bk)=> { 
+    return bk.publish >= 1961 && bk.genre==='Fiction'
+} )// using return b/c here is scope {}
+console.log(userbooks)
 
 
 
